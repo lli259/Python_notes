4d2323b31257e01fe02e1ed47f3a215efa47bb2f
lli259 <37081135+lli259@users.noreply.github.com>
2018-07-11 00:16:11 -0400

    Create replace.py

---
+++ replace.py
@@ -0,0 +1,16 @@
+import sys
+import re
+with open(sys.argv[1],'r') as f:
+	lines=f.read()
+	lines=re.sub(sys.argv[3],sys.argv[4],lines)
+	with open(sys.argv[2],'w') as f2:
+		f2.write(lines)
+
+'''
+
+# replace he110 in f1.txt with hello and store new content into f2.txt
+echo "Hey. he110 world" > f1.txt
+python replace.py f1.txt f2.txt he11O hello
+cat f2.txt
+rm f1.txt f2.txt
+'''
